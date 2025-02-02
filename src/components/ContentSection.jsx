fe72855e412c7a9e5b36b6f5eef7f6a4133790bb
CryptoPlaza <cryptoplaza.hq@gmail.com>
2025-02-02 11:55:04 -0500

    Add ContentSection component

---
+++ src/components/ContentSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContentSection = ({ children }) => {
+  return (
+    <main className="content-section">
+      {children}
+    </main>
+  );
+};
+
+export default ContentSection;
